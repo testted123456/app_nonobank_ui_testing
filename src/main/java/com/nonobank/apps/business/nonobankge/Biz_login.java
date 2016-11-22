5dc7608fb59597904554b9bd9629aee04a09a5a6
sunyueying <sunyueying@nonobank.com>
2016-11-22 14:00:28 +0800

    增加登录业务

---
+++ src/main/java/com/nonobank/apps/business/nonobankge/Biz_login.java
@@ -0,0 +1,19 @@
+package com.nonobank.apps.business.nonobankge;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.nonobank.apps.page.nonobankge.Page_login;
+import com.nonobank.apps.page.nonobankge.Page_register;
+import com.nonobank.apps.testcase.base.BaseCase;
+
+public class Biz_login extends BaseCase {
+	public static Logger logger = LogManager.getLogger(Biz_login.class);
+	Page_login page_login=new Page_login();
+	
+	public void login(Var_login var_login){
+		page_login.input_mobile(var_login.getMobile());
+		page_login.input_password(var_login.getPassword());
+		page_login.click_login();	
+	}
+}
---
+++ src/main/java/com/nonobank/apps/business/nonobankge/Var_login.java
@@ -0,0 +1,26 @@
+package com.nonobank.apps.business.nonobankge;
+
+public class Var_login {
+	private static String mobile;
+	private static String password;
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobilee(String mobile) {
+		this.mobile = mobile;
+	}
+	
+	public static String getPassword() {
+		return password;
+	}
+
+	public static void setPassword(String password) {
+		Var_login.password = password;
+	}
+	
+	
+	
+
+}
