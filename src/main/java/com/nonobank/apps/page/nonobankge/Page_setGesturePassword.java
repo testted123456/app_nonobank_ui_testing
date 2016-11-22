997b752076d168dffe739d19b884ffdd37f6260f
hanjunchao <hanjunchao@nonobank.com>
2016-11-22 09:55:09 +0800

    增加

--- src/main/java/com/nonobank/apps/page/nonobankge/Page_setGesturePassword.java
+++ src/main/java/com/nonobank/apps/page/nonobankge/Page_setGesturePassword.java
@@ -1,10 +1,27 @@
 package com.nonobank.apps.page.nonobankge;
 
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebElement;
+
 import com.nonobank.apps.page.base.BasePage;
 
 public class Page_setGesturePassword extends BasePage{
 	
-	
+	public void swipe_setGesturePassword(){
+		logger.info("[Page]设置手势密码。。。。。。");
+		WebElement element = objectRepository.getWebElement("大图标");
+		Point init_point = element.getLocation();
+		int x_point = init_point.getX();
+		int y_point = init_point.getY();
+		Dimension dimension = element.getSize();
+		int height = dimension.getHeight();
+		int width = dimension.getWidth();
+		int each_width = width/3;
+		int each_height = height/3;
+		appActions.swipe(startX, startY, endX, endY, ms);
+		
+	}
 	
 	
 }
