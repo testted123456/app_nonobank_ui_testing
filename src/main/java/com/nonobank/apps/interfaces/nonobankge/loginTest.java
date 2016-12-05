d115f90d563233842185cd4d72a0fcd269d3663d
hanjunchao <hanjunchao@nonobank.com>
2016-12-05 14:32:40 +0800

    增加

---
+++ src/main/java/com/nonobank/apps/interfaces/nonobankge/loginTest.java
@@ -0,0 +1,26 @@
+package com.nonobank.apps.interfaces.nonobankge;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import com.nonobank.apps.interfaces.util.SendRequest;
+
+public class loginTest {
+	public static Logger logger = LogManager.getLogger(loginTest.class);
+	public static List<HashMap<String, String>> params = new ArrayList<HashMap<String, String>>();
+	public static String url = "/msapi/user/login";
+	
+	public static String payRoute(String username,String password,String blackBox){
+		HashMap<String, String> requestParams = new HashMap<String, String>();
+		requestParams.put("password", password);
+		requestParams.put("terminal", "5");
+		requestParams.put("username", username);
+		requestParams.put("version", "4.5.0");
+		requestParams.put("blackBox", blackBox);
+		String response = SendRequest.httpCommonPost(url, requestParams);
+		return response;
+	}
+}
--- src/main/java/com/nonobank/apps/testcase/nonobankge/bankCard/BankCard.java
+++ src/main/java/com/nonobank/apps/testcase/nonobankge/bankCard/BankCard.java
@@ -2,18 +2,29 @@ package com.nonobank.apps.testcase.nonobankge.bankCard;
 
 import org.testng.annotations.Test;
 
+import com.nonobank.apps.business.nonobankge.Biz_common;
+import com.nonobank.apps.business.nonobankge.Biz_me;
 import com.nonobank.apps.business.nonobankge.Biz_register;
 import com.nonobank.apps.business.nonobankge.Var_register;
 import com.nonobank.apps.testcase.base.BaseCase;
 
 public class BankCard extends BaseCase{
 	Biz_register biz_register;
+	Biz_common biz_common;
+	Biz_me biz_me;
 	
 	@Test(dataProvider="dataSource")
 	public void test(Var_register var_register){
 		//注册
 		biz_register.register(var_register);
-		//
+		//实名认证
+		
+		
+		//点击我的
+		biz_common.click_me();
+		//点击银行卡管理
+		biz_me.click_bankcard();
+		
 		
 		
 		
